c15771c0119f9c65f6939d5297145fc034edb516
Gooi <samgooi4189@gmail.com>
2014-01-18 15:17:06 -0600

    implemented json display on homepage

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,12 +2,20 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('myApp.controllers', [])
+	.controller('PostController', ['$scope',  '$http', function($scope, $http) {
+		$http({method: 'GET', url: 'http://localhost:3000/posts.json'}).success(function(data){
+			$scope.posts = data;
+		});
+  	}])
+	.controller('MyCtrl1', [function(){
 
-  }])
-  .controller('MyCtrl2', [function() {
+	}])
+	.controller('MyCtrl2', [function(){
 
-  }]);
+	}]);
+
+
+
 
 
