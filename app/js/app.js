6fd81cbdfb35ed4a76f5e2ec6f1cd6a67d90c626
Gooi <samgooi4189@gmail.com>
2014-01-19 08:56:18 -0600

    Make everything beautiful

--- app/js/app.js
+++ app/js/app.js
@@ -13,5 +13,6 @@ var myApp = angular.module('myApp', [
 myApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/form', {templateUrl: 'form.html', controller: 'formController'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
